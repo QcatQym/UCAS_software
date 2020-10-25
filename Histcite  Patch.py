d8b61ae21367683795d9dad10989a195311282d1
QcatQym <54223257+QcatQym@users.noreply.github.com>
2020-10-25 14:40:36 +0800

    Update Histcite  Patch.py

--- Histcite  Patch.py
+++ Histcite  Patch.py
@@ -7,9 +7,7 @@ if os.path.isdir(cc):
 		filad=os.path.join(cc,fil)
 		with open(filad,'r',encoding='utf-8') as d:
 			fir=d.readlines()
-			print(fir[0][3:7])
 		if  not fir[0][3:7]=='Thom':
-			print(6)
 			os.remove(filad)
 			with open(filad,'w',encoding='utf-8') as dd:
 				dd.write('FN Thomson Reuters Web of Knowledge\n')
